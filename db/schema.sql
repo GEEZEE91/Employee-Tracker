e7ced9ade51afe4fa0488146a3872b1ca656087f
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2022-11-03 04:15:10 +0800

    fix schema syntex issue

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,33 @@ DROP DATABASE IF EXISTS ems_db;
 -- Creates the ems_db database --
 CREATE DATABASE ems_db;
 
-
 -- use ems_db database --
 USE ems_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-        FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-    manager_id, 
-        FOREIGN KEY (manager_id) REFERENCES employee(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-SELECT DATABASE();
+

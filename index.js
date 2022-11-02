382e4e74cdae61b0b62f5c41a9d7f95b588a41b5
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2022-11-03 04:13:08 +0800

    inquirer main menu and view dept, role & emp

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
-const express = require('express');
+// const express = require('express');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 // Express middleware
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
 // Connect to database
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
@@ -20,29 +20,154 @@ const db = mysql.createConnection(
     console.log(`Connected to the ems_db database.`)
   );
   
+  runApp();
+
+//Splash screen to show employee tracker
+function runApp() {
+  const display = ( `
+=== ╔════════════════╗ ===
+=== ║    EMPLOYEE    ║ ===
+=== ║    DATABASE    ║ ===
+=== ╚════════════════╝ ===
+  `);
+  console.log(display);
+  promptUser();
+}
+
+function promptUser() {
+  inquirer.prompt({
+      name: "action",
+      type: "list",
+      pageSize: 20,
+      message: "What would you like to do?",
+      choices: [
+        new inquirer.Separator('---------- DEPARTMENT OPERATIONS --------'),
+        "View all Departments",
+        "Add Department",
+        "Delete Department",
+        "View total BUDGET by Department",
+
+        new inquirer.Separator('---------- ROLE OPERATIONS --------------'),
+          "View all Roles",
+          "Add Role",
+          "Delete Role",
+
+          new inquirer.Separator('---------- EMPLOYEE OPERATIONS ----------'),
+
+          "View all Employees",
+          "View Employees by Manager",
+          "Add Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Delete Employee",
+
+          new inquirer.Separator(),
+          "Exit"
+      ]
+
+  }).then(function (answers) {
+      switch (answers.action) {
+        case "View all Departments":
+              viewDepartments();
+              break;
+        case "Add Department":
+                addDepartment();
+                break;
+        case "Delete Department":
+                deleteDepartment();
+                break;
+        case "View total BUDGET by Department":
+                viewTotalBudgetByDepartment();
+                 break;
 
 
+        case "View all Roles":
+              viewRoles();
+              break;
+        case "Add Role":
+                addRole();
+                break;
+        case "Delete Role":
+                deleteRole();
+                break;
 
 
+        case "View all Employees":
+              viewEmployees();
+              break;
+        case "View Employees by Manager":
+                viewEmployeesByManager();
+                break;
+          
+        case "Add Employee":
+              addEmployee();
+              break;
 
+        case "Update Employee Role":
+                updateEmployeeRole();
+                 break;
+          
+        case "Update Employee Manager":
+              updateEmployeeManager();
+              break;
+        
 
-
-
-
-
-
-
-
-
-
-
-
-  // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+          case "Delete Employee":
+              deleteEmployee();
+              break;
+       
+        
+          case "Exit":
+              connection.end();
+              break;
+      }
   });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+}
+
+
+//Select from department
+function viewDepartments() {
+  console.log(`Viewing all Departments`);
+  connection.query("select * from department", (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      promptUser();
   });
-  
+}
+
+//Select from role
+function viewRoles() {
+  console.log(`Viewing all roles`);
+  connection.query("select * from roles", (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      promptUser();
+  });
+}
+//Select from employee
+function viewEmployees() {
+  console.log(`Viewing all Employees`);
+  connection.query("select * from employee", (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      promptUser();
+  });
+}
+
+// INSERT INTO department
+function addDepartment() {
+  inquirer.prompt([
+      {
+          type: "input",
+          name: "deptName",
+          message: "What Department would you like to add?"
+      }
+  ]).then(function (res) {
+      // console.log(res);
+      connection.query("INSERT INTO department SET ?", { department_name: res.deptName }, (err, res) => {
+          if (err) throw err;
+          console.table(`Successfully added department`);
+          promptUser();
+      });
+  });
+}
